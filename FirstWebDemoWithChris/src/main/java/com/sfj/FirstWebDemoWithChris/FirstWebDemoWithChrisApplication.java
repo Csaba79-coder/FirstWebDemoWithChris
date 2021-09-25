47ecdd211d8dcd58a4e90059ae6e34b1650746f9
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2021-09-25 22:46:33 +0200

    Initial commit

---
+++ FirstWebDemoWithChris/src/main/java/com/sfj/FirstWebDemoWithChris/FirstWebDemoWithChrisApplication.java
@@ -0,0 +1,14 @@
+package com.sfj.FirstWebDemoWithChris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWebDemoWithChrisApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FirstWebDemoWithChrisApplication.class, args);
+	}
+
+}
---
+++ FirstWebDemoWithChris/src/test/java/com/sfj/FirstWebDemoWithChris/FirstWebDemoWithChrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfj.FirstWebDemoWithChris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWebDemoWithChrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
